66e8dac3bf874f3e401974d764f39778001d9ec6
OtnielJL <otnieljl@gmail.com>
2024-10-16 17:58:19 +0100

    database models

--- api/server.js
+++ api/server.js
@@ -1,12 +1,52 @@
+/**==========================================================
+ |
+ |              Imports
+ |
+ *==========================================================*/
+
 const express = require('express');
 const app = express();
 const port = 3000;
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('fantasydb', 'postgres', 'postgres', {
     host: 'localhost',
     dialect: 'postgres',
     logging: false
 });
+
+/**==========================================================
+ |
+ |              DB Models
+ |
+ *==========================================================*/
+const User = sequelize.define(
+    'User',
+    {
+      // Model attributes are defined here
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+       
+      },
+      email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    
+      },
+      password:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    
+      },
+    },
+    {
+       // Other model options go here
+    },
+  );
+
 
 
 // sequelize.authenticate().then(()=> console.log('success connection')).catch(()=> console.log('connection failed'));
